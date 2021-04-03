8606fd605925fd0a70887fcee1504678451a52ce
divyachhipani <divya.chhipani@gmail.com>
2021-04-03 16:48:17 +0100

    APIs

---
+++ backend/apis.py
@@ -0,0 +1,90 @@
+from flask import Flask, jsonify, request, render_template
+from flask_restful import Resource, Api
+import search_engine
+import json
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def start():
+    return "Server running.."
+
+@app.route("/search", methods=['GET'])
+def search():
+    body = request.get_json()
+    print('Search input:')
+    print(body)
+
+    #Request body
+    # {
+    #     "searchTerm": "Counter-Strike",
+    #     "boolOp" : 0,  # index of the list-item ['must', 'should', 'match']
+    #     "filterOp" : 0,  # 0 for 'lt', 1 for 'gt'
+    #     "categoryFilter": 0, #index of the list-item ['price', 'rating', 'owners']
+    #     "totalDocs": 10,
+    #     "needSort" : 1,    #if sorting is needed, needSort =1 else needSort = 0
+    #     "sortBy": ["NAME"],        # required only if needSort = 1
+    #     "isAscending":1,    # required only if needSort = 1
+    #     "needFilter" : 1, #if filting is needed, needFilter =1 else needFilter = 0
+    #     "categories": ['NAME', 'PRICE', 'RATING', 'SHORT_DESCRIPTION', 'OWNERS', 'TAGS'] # required only if needFilter = 1
+    # }
+
+    results = search_engine.search(body['searchTerm'], body['boolOp'], body['filterOp'], body['categoryFilter'], body['totalDocs'])
+
+    if body['needSort'] == 1:
+        results = search_engine.sorting(results, True, body['sortBy'], bool(body['isAscending']))
+
+    if body['needFilter'] == 1:
+        results = search_engine.filtering(results, True, body['categories'])
+
+    result = {"data": results }
+    return json.dumps(result, default=lambda results: json.loads(results.to_json()))
+
+@app.route("/sort", methods=['GET'])
+def sort():
+    body = request.get_json()
+    print('Sort input:')
+    print(body)
+
+    # Request body
+    # {   "docs":"{\"NAME\":{\"5\":\"AirMech Strike\",\"2\":\"Counter Agents\",\"3\":\"Counter Fight 3\",\"0\":\"Counter-Strike Nexon: Zombies\",\"1\":\"Counter-Strike: Global Offensive\",\"8\":\"Ember Strike\",\"7\":\"Fair Strike\",\"4\":\"Fantasy Strike\",\"6\":\"Fury Strike\",\"9\":\"Puzzle Strike\"},\"PRICE\":{\"5\":0.0,\"2\":0.0,\"3\":11.39,\"0\":0.0,\"1\":0.0,\"8\":0.0,\"7\":2.79,\"4\":14.99,\"6\":0.0,\"9\":10.99},\"RATING\":{\"5\":6200.0,\"2\":95.0,\"3\":4.0,\"0\":10003.0,\"1\":2242091.0,\"8\":639.0,\"7\":15.0,\"4\":190.0,\"6\":-6.0,\"9\":19.0},\"SHORT_DESCRIPTION\":{\"5\":\"AirMech\\u00ae Strike is a fast paced Action-RTS game that can be played online competitively or cooperatively. Earn Kudos and Experience in battle and unlock a wide collection of AirMechs and Units while you practice the perfect strategy to emerge victorious!\",\"2\":\"Counter Agents is a spy-themed, head-to-head multiplayer game where your objective is to sneak around guards, steal the briefcase, and make it to an exit point before your opponent!\",\"3\":\"Counter Fight is never dead! \\u201cCounter Fight 3\\u201d is a work simulation game where the player becomes a chef of a pizza store and provides meals to unique customers who appear one after another.\",\"0\":\"Counter-Strike Nexon: Zombies is a Free to Play MMOFPS offering competitive PvP and PvE action including content from the original Counter-Strike and all new game modes, maps, weapons, and hordes of Zombies!\",\"1\":\"Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, weapons, and game modes, and delivers updated versions of the classic CS content (de_dust2, etc.).\",\"8\":\"Fast head-to-head fighting like you\\u2019ve never seen.Welcome to Ember Strike. Join the first PVP fighter of its kind! Ember Strike is the perfect mix of speed and action with live, competitive, head-to-head battles.\",\"7\":\"Fair Strike is a game about brave pilots and their lethal celestial weapons fighting against terrorism and drug trafficking. So don't waste your time and jump into the cockpit of a modern attack helicopter and join a war against world terrorism. Your goal is simple - Kill them all!\",\"4\":\"A fighting game designed from the ground up to be so easy to control that even non-fighting game players can play it, yet deep enough to play in tournaments. Vibrant graphics and excellent online play.\",\"6\":\"Fury Strike is an 80s-themed 2.5D cel-shaded fighter game created by Full Sail University students in collaboration with Rooster Teeth.\",\"9\":\"Puzzle Strike is an online deckbuilding puzzle game from David Sirlin, lead designer of Puzzle Fighter HD Remix and creator of the Fantasy Strike universe. Improve your deck (of chips!) as the game unfolds and crush your opponents under a mountain of gems!\"},\"OWNERS\":{\"5\":1500000.0,\"2\":35000.0,\"3\":10000.0,\"0\":7500000.0,\"1\":75000000.0,\"8\":150000.0,\"7\":10000.0,\"4\":10000.0,\"6\":10000.0,\"9\":10000.0},\"TAGS\":{\"5\":\"action indie multiplayer strategy\",\"2\":\"indie casual\",\"3\":\"action indie casual simulation\",\"0\":\"action multiplayer singleplayer\",\"1\":\"action multiplayer strategy\",\"8\":\"action indie\",\"7\":\"action simulation\",\"4\":\"action indie\",\"6\":\"action\",\"9\":\"indie strategy\"}}",
+    #     "sortBy": ["NAME"],
+    #     "isAscending":1
+    # }
+    #categories = ['NAME', 'PRICE', 'RATING', 'SHORT_DESCRIPTION', 'OWNERS', 'TAGS']
+
+    docs_json = json.loads(body['docs'])
+    df = pd.DataFrame.from_dict(docs_json, orient="columns")
+    results = search_engine.sorting(df, True, body['sortBy'],body['isAscending'])
+
+    result = {"data": results }
+    return json.dumps(result, default=lambda results: json.loads(results.to_json()))
+
+@app.route("/filter", methods=['GET'])
+def filter():
+    body = request.get_json()
+    print('Filter input:')
+    print(body)
+    # Request body
+    # {
+    #     "docs": "{\"NAME\":{\"5\":\"AirMech Strike\",\"2\":\"Counter Agents\",\"3\":\"Counter Fight 3\",\"0\":\"Counter-Strike Nexon: Zombies\",\"1\":\"Counter-Strike: Global Offensive\",\"8\":\"Ember Strike\",\"7\":\"Fair Strike\",\"4\":\"Fantasy Strike\",\"6\":\"Fury Strike\",\"9\":\"Puzzle Strike\"},\"PRICE\":{\"5\":0.0,\"2\":0.0,\"3\":11.39,\"0\":0.0,\"1\":0.0,\"8\":0.0,\"7\":2.79,\"4\":14.99,\"6\":0.0,\"9\":10.99},\"RATING\":{\"5\":6200.0,\"2\":95.0,\"3\":4.0,\"0\":10003.0,\"1\":2242091.0,\"8\":639.0,\"7\":15.0,\"4\":190.0,\"6\":-6.0,\"9\":19.0},\"SHORT_DESCRIPTION\":{\"5\":\"AirMech\\u00ae Strike is a fast paced Action-RTS game that can be played online competitively or cooperatively. Earn Kudos and Experience in battle and unlock a wide collection of AirMechs and Units while you practice the perfect strategy to emerge victorious!\",\"2\":\"Counter Agents is a spy-themed, head-to-head multiplayer game where your objective is to sneak around guards, steal the briefcase, and make it to an exit point before your opponent!\",\"3\":\"Counter Fight is never dead! \\u201cCounter Fight 3\\u201d is a work simulation game where the player becomes a chef of a pizza store and provides meals to unique customers who appear one after another.\",\"0\":\"Counter-Strike Nexon: Zombies is a Free to Play MMOFPS offering competitive PvP and PvE action including content from the original Counter-Strike and all new game modes, maps, weapons, and hordes of Zombies!\",\"1\":\"Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, weapons, and game modes, and delivers updated versions of the classic CS content (de_dust2, etc.).\",\"8\":\"Fast head-to-head fighting like you\\u2019ve never seen.Welcome to Ember Strike. Join the first PVP fighter of its kind! Ember Strike is the perfect mix of speed and action with live, competitive, head-to-head battles.\",\"7\":\"Fair Strike is a game about brave pilots and their lethal celestial weapons fighting against terrorism and drug trafficking. So don't waste your time and jump into the cockpit of a modern attack helicopter and join a war against world terrorism. Your goal is simple - Kill them all!\",\"4\":\"A fighting game designed from the ground up to be so easy to control that even non-fighting game players can play it, yet deep enough to play in tournaments. Vibrant graphics and excellent online play.\",\"6\":\"Fury Strike is an 80s-themed 2.5D cel-shaded fighter game created by Full Sail University students in collaboration with Rooster Teeth.\",\"9\":\"Puzzle Strike is an online deckbuilding puzzle game from David Sirlin, lead designer of Puzzle Fighter HD Remix and creator of the Fantasy Strike universe. Improve your deck (of chips!) as the game unfolds and crush your opponents under a mountain of gems!\"},\"OWNERS\":{\"5\":1500000.0,\"2\":35000.0,\"3\":10000.0,\"0\":7500000.0,\"1\":75000000.0,\"8\":150000.0,\"7\":10000.0,\"4\":10000.0,\"6\":10000.0,\"9\":10000.0},\"TAGS\":{\"5\":\"action indie multiplayer strategy\",\"2\":\"indie casual\",\"3\":\"action indie casual simulation\",\"0\":\"action multiplayer singleplayer\",\"1\":\"action multiplayer strategy\",\"8\":\"action indie\",\"7\":\"action simulation\",\"4\":\"action indie\",\"6\":\"action\",\"9\":\"indie strategy\"}}",
+    #     "sortBy": ["NAME"],
+    #     "isAscending": 1
+    #     }
+    #categories = ['NAME', 'PRICE', 'RATING', 'SHORT_DESCRIPTION', 'OWNERS', 'TAGS']
+
+    docs_json = json.loads(body['docs'])
+    df = pd.DataFrame.from_dict(docs_json,orient="columns")
+
+    results = search_engine.filtering(df, True, body['categories'])
+
+    result = {"data": results}
+    return json.dumps(result, default=lambda results: json.loads(results.to_json()))
+
+if __name__ == '__main__':
+    search_engine.main()
+    app.run(debug=True)
+
+
---
+++ backend/search_engine.py
@@ -0,0 +1,253 @@
+# pip install pandas
+# pip install elasticsearch
+# using Python 3.9.0
+# make sure to run elasticsearch.bat
+
+from timeit import default_timer as timer
+import pandas as pd
+import csv
+from elasticsearch import Elasticsearch, helpers
+import re as re
+
+steamAppsIndex = 'steamapps'
+es = Elasticsearch()
+
+def preprocess(steamtPath, tagsPath, descriptionPath, outputPath):
+    print('preprocessing dataset...')
+    start = timer()
+
+    fields = ['appid', 'name', 'positive_ratings',
+              'negative_ratings', 'owners', 'price']
+    df = pd.read_csv(steamtPath, index_col='appid', usecols=fields)
+
+    # clean owners into single number (center of band)
+    df['owners'] = (df['owners'].str.split('-').str[0].astype(int) +
+                    df['owners'].str.split('-').str[1].astype(int)) // 2
+
+    # join positive and negative ratings into single score
+    df['rating'] = df['positive_ratings'] - df['negative_ratings']
+    df = df.drop(['positive_ratings', 'negative_ratings'], axis=1)
+
+    tags = ['action', 'indie', 'adventure', 'multiplayer', 'singleplayer',
+            'casual', 'rpg', 'strategy', 'open_world', 'simulation']
+
+    fields = ['appid'] + tags
+    tagsDf = pd.read_csv(tagsPath, index_col='appid', usecols=fields)
+
+    tagsDf['tagCount'] = tagsDf.sum(axis=1, skipna=True)
+
+    # assign boolean value for each tag (ignoring appid field)
+    userAgreementTreshold = 0.1
+    for tag in tags:
+        df[tag] = tagsDf[tag] > userAgreementTreshold * tagsDf['tagCount']
+
+    # get game descriptions
+    fields = ['steam_appid', 'detailed_description',
+              'about_the_game', 'short_description']
+    descDf = pd.read_csv(
+        descriptionPath, index_col='steam_appid', usecols=fields)
+
+    # remove html tags
+    df['about_the_game'] = descDf['about_the_game'].apply(
+        lambda x: re.sub('<.*?>', '', x))
+    df['short_description'] = descDf['short_description'].apply(
+        lambda x: re.sub('<.*?>', '', x))
+    df['detailed_description'] = descDf['detailed_description'].apply(
+        lambda x: re.sub('<.*?>', '', x))
+
+    print(df)
+    print(f'finished preprocessing in {timer() - start}s')
+    df.to_csv(path_or_buf=outputPath)
+
+def indexSteamApps(es, datasetPath):
+    print(f'indexing csv {datasetPath}...')
+
+    # erase previous index
+    es.indices.delete(index=steamAppsIndex, ignore=[
+        400, 404])
+    start = timer()
+
+    with open(datasetPath, encoding='utf8', errors='replace') as f:
+        reader = csv.DictReader(f)
+        # response = helpers.bulk(es, reader, index=steamAppsIndex)
+        for success, info in helpers.parallel_bulk(es, reader, index=steamAppsIndex):
+            if not success:
+                print(f'A document failed: {info}')
+
+    print(f'finished indexing in {timer() - start}s')
+
+    es.indices.refresh(index=steamAppsIndex)
+
+def query(es, settings, total_docs):
+    print('')
+    print(settings)
+    start = timer()
+
+    result = es.search(index=steamAppsIndex, body={"query": settings}, size=total_docs)  # returns a dictionary
+
+    print('')
+    print(f"Got {result['hits']['total']['value']} Hits in {timer() - start}s:")
+
+    print('')
+
+    # lists for data storage
+    # text based
+    name = []
+    short_description = []
+    detailed_description = []
+    about_the_game = []
+
+    # numerics
+    price = []
+    rating = []
+    # appid = []
+    owners = []
+
+    # tags
+    tags = []
+
+    for hit in result['hits']['hits']:  # extract entries from a list
+
+        tag_temp = []
+        # print entries and add to the relevant lists declared above
+        # print(f'{hit["_source"]}')
+        name.append(hit["_source"]["name"])
+        price.append(float(hit["_source"]["price"]))
+        rating.append(float(hit["_source"]["rating"]))
+        # appid.append(float(hit["_source"]["appid"]))
+        owners.append(float(hit["_source"]["owners"]))
+        short_description.append(hit["_source"]["short_description"])
+        detailed_description.append(hit["_source"]["detailed_description"])
+        about_the_game.append((hit["_source"]["about_the_game"]))
+
+        # get tags and put them into a string
+        if (hit["_source"]["action"]) == 'True':
+            tag_temp.append('action')
+        if (hit["_source"]["indie"]) == 'True':
+            tag_temp.append('indie')
+        if (hit["_source"]["adventure"]) == 'True':
+            tag_temp.append('adventure')
+        if (hit["_source"]["multiplayer"]) == 'True':
+            tag_temp.append('multiplayer')
+        if (hit["_source"]["singleplayer"]) == 'True':
+            tag_temp.append('singleplayer')
+        if (hit["_source"]["casual"]) == 'True':
+            tag_temp.append('casual')
+        if (hit["_source"]["rpg"]) == 'True':
+            tag_temp.append('rpg')
+        if (hit["_source"]["strategy"]) == 'True':
+            tag_temp.append('strategy')
+        if (hit["_source"]["open_world"]) == 'True':
+            tag_temp.append('open_world')
+        if (hit["_source"]["simulation"]) == 'True':
+            tag_temp.append('simulation')
+        tags.append(' '.join([str(item) for item in tag_temp]))
+
+    dict = {'NAME': name, 'PRICE': price, 'RATING': rating, 'SHORT_DESCRIPTION': short_description, 'OWNERS': owners,
+            'TAGS': tags}  # store lists into a dictionary
+    result = pd.DataFrame(dict)  # output dictionary as a dataframe and return the dataframe
+
+    return result
+
+def sorting(df, en, sorting_criteria, ascending):
+    """
+    :param df: an input dataframe
+    :param sorting_criteria: the parameters by which you wish to sort
+    :param ascending: boolean value
+    :return: sorted dataframe
+    """
+
+    print(sorting_criteria)
+    if en:
+        df = df.sort_values(by=sorting_criteria, ascending=ascending)
+    return df
+
+def filtering(df, en, items):
+    """
+    :param df: an input dataframe
+    :param items: the columns you would like to keep
+    :return: dataframe with specified columns
+    """
+    if en:
+        df = df.filter(items=items)
+    return df
+
+def substring_search(df, en, category, substring):
+    """
+    :param df: input data frame
+    :param category: the column you would like to search a string for
+    :param substring: the string to check for
+    :return: dataframe with entries containing specified string in specified column
+    """
+    if en:
+        df = df[df[category].str.contains(substring)]
+    return df
+
+def print_the_data(result, sorted_data, filtered_data, data_with_string):
+    pd.set_option('max_columns', 6)  # this is to print out all columns
+    print('============')
+    print('Sorted Data')
+    print('============')
+    print(sorted_data)
+    print('=============')
+    print('Filtered Data')
+    print('=============')
+    print(filtered_data)
+    print('=================')
+    print('Data With String')
+    print('=================')
+    print(data_with_string)
+
+def generate_settings(selection_index, title, bool_op_index, categories_index, filter_operation_index, threshold):
+    # test query
+    bool_op = ['must', 'should', 'match']
+    filter_operation = ['lt', 'gt']
+    categories = ['price', 'rating', 'owners']
+
+    print(type(selection_index))
+    selection_index = selection_index % 3
+
+    if selection_index == 0:
+        # print('MATCH: ', title)
+        settings = {"match": {'name': title}}  # match operator
+    elif selection_index == 1:
+        # print('BOOL: ', bool_op[bool_op_index], title)
+        settings = {'bool': {bool_op[bool_op_index]: [{'match': {'name': title}}]}}  # bool must operator
+    elif selection_index == 2:
+        # print('BOOL: ', bool_op[bool_op_index], title, categories[categories_index], filter_operation[filter_operation_index], threshold)
+        settings = {'bool': {bool_op[bool_op_index]: {'match': {'name': title}}, "filter": {"range": {
+            categories[categories_index]: {filter_operation[filter_operation_index]: threshold}}}}}  # filter operation
+    else:
+        print('No Instruction Found')
+    return settings
+
+def main():
+    # preprocess data
+    steamtPath = 'data/steam.csv'
+    tagsPath = 'data/steamspy_tag_data.csv'
+    descriptionPath = 'data/steam_description_data.csv'
+    datasetPath = 'temp/dataset.csv'
+    preprocess(steamtPath, tagsPath, descriptionPath, outputPath=datasetPath)
+
+    # index/update steamapps from csv file
+    indexSteamApps(es, datasetPath)
+
+# Search function as called by APIs
+def search(searchTerm, boolOp, filterOp, categoryFilter, totalDocs):
+    print('====NEW SEARCH =====')
+    selection_index = 2
+    title = searchTerm
+    bool_op_index = boolOp
+    categories_index = categoryFilter
+    filter_operation_index = filterOp
+    threshold = 3
+    total_docs = totalDocs
+
+    settings = generate_settings(selection_index, title, bool_op_index, categories_index, filter_operation_index,
+                                     threshold)
+
+    return query(es, settings, total_docs)
+
+if __name__ == "__search_engine__":
+    # execute only if run as a script
+    main()
